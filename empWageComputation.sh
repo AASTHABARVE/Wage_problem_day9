3f7eb6533d68677c44b4c0c23cb8337183175c41
aastha <aasthabarve0@gmail.com>
2023-03-21 14:17:27 +0530

    add switch case program

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,11 @@
-
+echo "1st code"
 
 echo "Welcome to Employee Wage Computation Program";
 
 
-
+echo "2nd code"
 ispresent=1;
 randomcheck="$((RANDOM%2))";
-
-
-
 if [ $ispresent -eq $randomcheck ];
 then
 echo "Employee is present";
@@ -21,6 +18,8 @@ fi
 
 
 
+
+echo "3rd code"
 ispresentt=1;
 randomcheckk="$((RANDOM%2))";
 
@@ -37,3 +36,27 @@ else
 salary=0;
 
 fi
+
+echo "code 4"
+
+isfulltime=2;
+isparttime=1;
+empcheck="$((RANDOM%3))";
+emprateperhr=20;
+
+case $empcheck in
+       $isfulltime)
+       emphr=8
+        ;;
+
+       $isparttime)
+       emphr=4
+        ;;
+
+       *)
+
+salary=$(($emphr*$emprateperhr));
+echo "$salary"
+
+esac
+
